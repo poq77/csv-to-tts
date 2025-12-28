6e2af6f3a5ab07ee426c422fcaccbc8d795d381c
poq77 <509531106@qq.com>
2025-12-29 00:43:09 +0800

    update generate function

--- core/generate_from_input_json.py
+++ core/generate_from_input_json.py
@@ -12,6 +12,29 @@ def generate_audio(text: str, path: str, speed: float=1, voice: str='af_heart') 
         break # current support only one paragraph
 
 def generate_from_json(input_json:str):
+    # add databse folder when not exists
+    audio_path = Path("./database/audio")
+    if not audio_path.exists():
+        audio_path.mkdir(parents=True, exist_ok=True)
+    
+    if not audio_path.is_dir():
+        raise Exception("Cannot create database")
+
+    manifest = json.loads(input_json)
+
+    for entry in manifest["data"]:
+        for k in entry:
+            text = entry[k]["text"]
+            filename = hashlib.md5(f"{text}{manifest["speed"]}{manifest["voice"]}".encode('utf-8')).hexdigest()
+            path_str = audio_path / f"{filename}.wav"
+            print(path_str)
+            generate_audio(text, path=path_str, speed=manifest["speed"], voice=manifest["voice"])
+            entry[k]["audio"] = str(path_str.absolute())
+    
+    with open(f"./database/{manifest["name"]}.json", "w") as f:
+        json.dump(manifest, f)
+
+if __name__ == "__main__":
     input_json = """
         {
             "name": "example",
@@ -36,28 +59,4 @@ def generate_from_json(input_json:str):
                 }
             ]
         }"""
-
-    # add databse folder when not exists
-    audio_path = Path("./database/audio")
-    if not audio_path.exists():
-        audio_path.mkdir(parents=True, exist_ok=True)
-    
-    if not audio_path.is_dir():
-        raise Exception("Cannot create database")
-
-    manifest = json.loads(input_json)
-
-    for entry in manifest["data"]:
-        for k in entry:
-            text = entry[k]["text"]
-            filename = hashlib.md5(f"{text}{manifest["speed"]}{manifest["voice"]}".encode('utf-8')).hexdigest()
-            path_str = audio_path / f"{filename}.wav"
-            print(path_str)
-            generate_audio(text, path=path_str, speed=manifest["speed"], voice=manifest["voice"])
-            entry[k]["audio"] = str(path_str.absolute())
-    
-    with open(f"./database/{manifest["name"]}.json", "w") as f:
-        json.dump(manifest, f)
-
-if __name__ == "__main__":
-    generate_from_json("")
+    generate_from_json(input_json)
